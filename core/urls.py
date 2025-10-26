6cd5955c569c7b683f613322f01354cb9b30f6fb
Kayke Mendes <kaykemendes445@gmail.com>
2025-10-25 22:00:06 -0300

    Alteraçoes basicas

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('accounts/signup/', views.signup_view, name='signup'),
+    path('accounts/login/', views.login_view, name='login'),
+    path('accounts/logout/', views.logout_view, name='logout'),
     path('conversao/', views.conversao_view, name='conversao'),
-    
-    
-    
-   
 ]
